5551b254106305bf47fd54af27ab13d4e962a9e1
Cloudsanjay <cloud.sanjaysingh@gmail.com>
2025-08-10 00:55:00 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,10 +1,5 @@
 terraform {
-backend "azurerm" {
-  resource_group_name  = "sanjayrg"
-  storage_account_name = "sanjaysingraur3"
-  container_name       = "sanjaycontainer3"
-  key                  = "mango.tfstate"
-}
+backend "azurerm" {}
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"

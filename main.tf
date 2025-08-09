7d1b3976ecc3c19ce2c62a3467afda57e1646268
Cloudsanjay <cloud.sanjaysingh@gmail.com>
2025-08-10 01:04:51 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -18,10 +18,4 @@ resource "azurerm_resource_group" "mango" {
   location = "West Europe"
 }
 
-resource "azurerm_storage_account" "mangostg" {
-  name                     = "mangostorage"
-  resource_group_name      = azurerm_resource_group.mango.name
-  location                 = azurerm_resource_group.mango.location
-  account_tier             = "Standard"
-  account_replication_type = "LRS"
-}
+

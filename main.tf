8ee83f67c6526ef0fe5ffaaca8ac71ca50350aa0
Telecom <SanjaySingh@chandankumar02731gmail.onmicrosoft.com>
2025-08-09 23:49:19 +0530

    rg And Storage added

---
+++ main.tf
@@ -0,0 +1,27 @@
+terraform {
+backend "azurerm" {}
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "4.38.1"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+subscription_id = "9a19641f-a0da-455d-9b90-51dc0d0cdfee"
+}
+
+resource "azurerm_resource_group" "mango" {
+  name     = "mango-rg"
+  location = "West Europe"
+}
+
+resource "azurerm_storage_account" "mangostg" {
+  name                     = "mangostorage"
+  resource_group_name      = azurerm_resource_group.mango.name
+  location                 = azurerm_resource_group.mango.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+}
